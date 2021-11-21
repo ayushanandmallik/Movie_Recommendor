a5dadf9052c9a394a0be19df0a1f685cbc443cc2
Ayush Anand Mallik <ayushmallik6342@gmail.com>
2021-11-21 14:47:21 +0530

    udpates

--- movie_recommendor/urls.py
+++ movie_recommendor/urls.py
@@ -28,5 +28,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('result', views.result, name='result'),
+    path('seemore/<str:movie_name>', views.seemore, name='seemore'),
+    path('genre', views.genre, name='genre'),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
--- movie_recommendor/views.py
+++ movie_recommendor/views.py
@@ -6,6 +6,7 @@ import pandas as pd
 def home(request):
     return render(request,'index.html')
 
+
 def result(request):
     watched= request.GET['watched']
 
@@ -36,5 +37,37 @@ def result(request):
                                           'imdbrating':imdbrating,'boxoffice':boxoffice,'recommended_movies':rec_mov})
 
 
+def seemore(request, movie_name):
+    watched= movie_name
+    context={'movie_name':movie_name}
+
+    searched_movie= imdb.search(watched)
+    s_title= searched_movie[0]['original_title']
+    s_overview= searched_movie[0]['overview']
+    s_poster= 'https://image.tmdb.org/t/p/w500'+searched_movie[0]['poster_path']
+    s_released= searched_movie[0]['release_date']
+
+    mov= imdb.movieinfo(s_title)
+    genre= mov['genre']
+    director= mov['director']
+    awards= mov['awards']
+    rotten_tomatoes= mov['ratings'][1]['Value']
+    metacritic= mov['metascore']
+    imdbrating= mov['imdbrating']
+    boxoffice= mov['boxoffice']
+
+    recommended_movies= imdb.rec(s_title)
+    rec_mov= []
+    for movies in recommended_movies:
+        mov= imdb.movieinfo(movies['original_title'])
+        rec_mov.append(mov)
 
 
+    return  render(request,'result.html',{'title':s_title,'release_date':s_released,'genre':genre,'director':director,'plot':s_overview,
+                                          'awards':awards,'poster':s_poster,'rotten_tomatoes':rotten_tomatoes,'metacritic':metacritic,
+                                          'imdbrating':imdbrating,'boxoffice':boxoffice,'recommended_movies':rec_mov})
+
+
+def genre(request):
+    g= request.GET['genre']
+    return render(request, 'genre.html',{'genre':g})
